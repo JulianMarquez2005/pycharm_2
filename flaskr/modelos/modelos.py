d9a32b24b76873c75657a2bb70d357ea2f9a949c
JulianMarquez2005 <119985131+JulianMarquez2005@users.noreply.github.com>
2023-08-28 18:48:49 -0500

    Add files via upload
    subo ejercicio completo completamente funcional con todas las clases

---
+++ flaskr/modelos/modelos.py
@@ -0,0 +1,41 @@
+import enum
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cancion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
+
+
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(128))
+    contrasena = db.Column(db.String(128))
+    albunes = db.relationship('Album', cascade='all, delete, delete-orphan')
+
+
+class Medio(enum.Enum):
+    DISCO = 1
+    CASETE = 2
+    CD = 3
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    anio = db.Column(db.Integer)
+    description = db.Column(db.String(100))
+    medio = db.Column(db.Enum(Medio))
+    id_usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"))
+    __tables_args__=(db.UniqueConstraint("usuario","titulo", name="titulo_unico_album"),)
+
+
+
+
+
