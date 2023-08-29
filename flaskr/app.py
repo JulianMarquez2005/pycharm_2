725dfa24532867de625a114c084db27cae4375ba
JulianMarquez2005 <119985131+JulianMarquez2005@users.noreply.github.com>
2023-08-29 15:21:55 -0500

    taller

---
+++ flaskr/app.py
@@ -0,0 +1,20 @@
+from flaskr import create_app
+from .modelos import db, Usuario, Album, Medio
+from .modelos import AlbumSchema
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+
+with app.app_context():
+    Album_Schema = AlbumSchema()
+    A = Album(titulo='Chocoaventuras', anio='2023', description='Album de pruebitas varias', medio=Medio.CD)
+    db.session.add(A)
+    db.session.commit()
+    print([Album_Schema.dumps(album) for album in Album.query.all()])
+
+
